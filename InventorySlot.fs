b7515d458c1ef81c7724fc207bda5b6da8eb49d4
mattnewport <matt@mattnewport.com>
2021-08-04 13:14:58 -0700

    Added inventory slot component

---
+++ InventorySlot.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharpJamCode
+
+open UnityEngine
+
+type InventorySlot() = 
+    inherit MonoBehaviour()
+    [<DefaultValue; SerializeField>]
+    val mutable private prefab : GameObject
+    [<DefaultValue>]
+    val mutable private item : GameObject
+    member private this.Update() =
+        if this.item = Unchecked.defaultof<GameObject>
+        then 
+            let newItem = GameObject.Instantiate<GameObject>(this.prefab, this.transform)
+            newItem.transform.localPosition <- Vector3.zero
+            newItem.transform.localRotation <- Quaternion.identity
+            this.item <- newItem
+        else
+            if this.item.transform.parent <> this.transform
+            then this.item <- null
